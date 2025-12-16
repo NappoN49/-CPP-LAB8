935360ecba27a03ab54ee1825e3a1c314a9101ac
NappoN49 <parawin39228@gmail.com>
2025-12-16 15:20:37 +0700

    lab8_3 complete

--- lab8_3.cpp
+++ lab8_3.cpp
@@ -1,13 +1,34 @@
-/*
-"Enter your age: "
-"Enter your height: "
-"Enter your bounty: "
-"Your character = "
-"Zoro"
-"Sanji"
-"Usopp"
-"Chopper"
-"Franky"
-"Brook"
-"Jinbe"
-*/
+#include<iostream>
+#include<string>
+#include<cmath>
+using namespace std;
+
+int main(){
+    string character="";
+    int age;
+    cout<<"Enter your age: ";cin>>age;
+    if(age<=25){
+        int height;
+        cout<<"Enter your height: ";
+        cin>>height;
+        if(height<100)character = "Chopper";
+        else if(height<180)character = "Usopp";
+        else{
+            long long bounty;
+            cout<<"Enter your bounty: ";cin>>bounty;
+            if(bounty<=11e8)character = "Sanji";
+            else character = "Zoro";
+        }
+    }
+    else{
+       if(age<=60){
+            long long bounty;
+            cout<<"Enter your bounty: ";cin>>bounty;
+            if(bounty<=5e8)character = "Franky";
+            else character = "Jinbe";
+       }
+       else character = "Brook";
+    }
+    cout<<"Your character = "<<character;
+    return 0;
+}
